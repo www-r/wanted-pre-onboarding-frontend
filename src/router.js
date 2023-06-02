5ebd9629981ed306f56b87ab002a744f5a3d25eb
youngeun <jessicak0828@gmail.com>
2023-06-02 21:09:01 +0900

    feat: 라우터 추가

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import TodolistsPage from "./pages/TodolistsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "/signin",
+    element: <SignInPage/>
+
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage/>
+
+  },
+  {
+    path: "/todolists",
+    element: <TodolistsPage/>
+
+  },
+  // {
+  //   path: *,
+  //   element: <NotFoundPage/>
+  // }
+]);

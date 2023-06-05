4fc12f2201fb0e58154355a9bccb1c65c668d9e2
youngeun <jessicak0828@gmail.com>
2023-06-05 17:49:11 +0900

    refactor: Router 위치 이동

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <RouterProvider router={router} />
     <App />
   </>
 );

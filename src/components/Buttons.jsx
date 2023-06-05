770743eca6160ab7e0047c3ec6f7c84d37d7c41a
youngeun <jessicak0828@gmail.com>
2023-06-05 16:28:37 +0900

    delete:

--- src/components/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import * as S from '../styles/Buttons.styles'
-
-export function PageButton({text}) {
-  return (
-    <S.Button><Link to={'/signin'}>{text}</Link></S.Button>
-  )
-}
--- src/components/Buttons/SubmitButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SubmitButton({text, email, password}) {
-  return (
-    <button disabled={email && password ? false : true}>{text}</button>
-  )
-}

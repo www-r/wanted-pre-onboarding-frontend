54d9b332a61d56d387e525ca2aa97098792924e4
youngeun <jessicak0828@gmail.com>
2023-06-08 04:13:15 +0900

    fix: todolists 가 빈 배열일 때 에러 고침

--- src/components/TodosContainer.jsx
+++ src/components/TodosContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
+import * as S from '../styles/TodosContainer.style'
 
 export default function TodosContainer({todolists, setTodolists, getTodolists}) {
-  // useEffect(()=>{getTodolists()},[todolists])
   return (
-    <ul>
-      { todolists.map((todo)=> <Todo key={todo.id} id={todo.id} todo={todo.todo} isCompleted={todo.isCompleted} userId={todo.userId} getTodolists={getTodolists}/>)}
-    </ul>
+    <S.TodosContainer>
+      { todolists?.map((todo)=> <Todo key={todo.id} id={todo.id} todo={todo.todo} isCompleted={todo.isCompleted} todolists={todolists} setTodolists={setTodolists} getTodolists={getTodolists}/>)}
+    </S.TodosContainer>
   )
 }

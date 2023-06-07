b3d7d6d66bc54b421729d6f8b7889a1d0132359e
youngeun <jessicak0828@gmail.com>
2023-06-07 17:46:11 +0900

    feat: api 요청 함수 에러 핸들링

--- src/functions/api.js
+++ src/functions/api.js
@@ -4,65 +4,94 @@ const instance = axios.create({
   baseURL: "https://www.pre-onboarding-selection-task.shop",
   headers: {"Content-Type": "application/json",}
 });
-const accessToken = localStorage.getItem('access_token')
+
 
 export const postSignIn = async(email, password) => {
-  const response = await instance.post('/auth/signin', {
+  try {
+    const response = await instance.post('/auth/signin', {
       "email": email,
       "password": password
   })
-  const accessToken = response.data.access_token;
-  console.log(accessToken);
-  localStorage.setItem("access_token",accessToken );
+  console.log('access_token:',  response.data.access_token);
+  localStorage.setItem("access_token", response.data.access_token );
+  console.log('signin:', response)
+  return response
+  } catch (error) {
+    console.error(error)
+  }
+  
 }
 
 export const postSignUp = async(email, password) => {
-  const response = await instance.post('auth/signup', {
+  try {
+    const response = await instance.post('/auth/signup', {
       "email": email,
       "password": password
   })
-  console.log(response)
+  console.log('signup:',response)
+  return response
+  } catch (error) {
+    console.error(error)
+  }
+ 
 }
 
 //
 export const createTodo = async(todo) => {
   instance.defaults.headers = {
     "Content-Type": "application/json",
-    "Authorization": `Bearer ${accessToken}`
+    "Authorization": `Bearer ${localStorage.getItem("access_token")}`
   }
+ try {
   const response = await instance.post('/todos', {
     "todo" : todo,
   })
-console.log('create:',response)
+console.log('create:',response.data)
+return response.data
+ } catch (error) {
+  console.log(error)
+ }
 }
 
 export const getTodos = async() => {
   instance.defaults.headers = {
-    "Authorization": `Bearer ${accessToken}`
+    "Authorization": `Bearer ${localStorage.getItem('access_token')}`
   }
+try {
   const response = await instance.get('/todos')
   console.log('get:',response.data)
   return response.data
+} catch (error) {
+  console.log(error)
+}
 }
 
 export const updateTodo = async (id, todo, isCompleted) => {
   instance.defaults.headers = {
     "Content-Type": "application/json",
-    "Authorization": `Bearer ${accessToken}`
+    "Authorization": `Bearer ${localStorage.getItem('access_token')}`
   }
+try {
   const response = await instance.put(`/todos/${id}`, {
     "todo" : todo,
     "isCompleted": isCompleted,
   })
-  console.log('update:',response)
+  console.log('update:',response.data)
   return response.data
+} catch (error) {
+  console.log(error)
+}
 }
 
 export const deleteTodo = async(id) => {
   instance.defaults.headers = {
-    "Authorization": `Bearer ${accessToken}`
+    "Authorization": `Bearer ${localStorage.getItem('access_token')}`
   }
+try {
   const response = await instance.delete(`/todos/${id}`)
-  console.log('delete:',response)
-  return response
+  console.log('delete:',response.data)
+  return response.data
+} catch (error) {
+  console.log(error)
+}
 }

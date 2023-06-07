f8a3171655e4a2989042b9daa51c555cc20d5bb1
youngeun <jessicak0828@gmail.com>
2023-06-07 17:45:08 +0900

    refactor: RouterProvider 위치변경 App.jsx 내 -> index.js 내

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React , {useState, useEffect} from "react";
+import React  from "react";
 import { Reset } from "styled-reset";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import { getTodos } from "./functions/api";
 
 
 function App() {
-  const [todolists, setTodolists] = useState([])
-  const getTodolists = async() => {
-    const todolists = await getTodos().data
-    setTodolists(todolists)
-  }
-  useEffect(()=>{getTodolists()},[])
+
   return (
     <>
     <Reset/>
-    <RouterProvider router={router} todolists={todolists}/>
+   
       <div className="App">
    
       </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+ <RouterProvider router={router}/>
     <App />
   </>
 );

52ae55831f2957a62825a5c52e40475f27f1d4c9
youngeun <jessicak0828@gmail.com>
2023-06-08 04:16:36 +0900

    feat: 회원가입 완료 후 로그인 페이지로 이동

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import Background from '../components/Background'
+import Container from '../components/Container'
 import Sign from '../components/Sign'
 
 export default function SignUpPage() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(localStorage.getItem('access_token')){
+      navigate('/todo')
+    }
+  },[])
 
   return (
-    <>
-    <Sign title={'회원가입'} action={'signup'}/>
-    </>
+    <Background>
+      <Container>
+        <Sign title={'회원가입'} action={'signup'}/>
+      </Container>
+    </Background>
    
   )
 }

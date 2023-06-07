f62c4a2a56ae3aa6360ee6c3030aed4c09d16064
youngeun <jessicak0828@gmail.com>
2023-06-07 17:49:17 +0900

    fix: 로그인한 계정 바뀔 때 마다 localStorage 에서 새로운 access_token 가져올 수 있게

--- src/router.js
+++ src/router.js
@@ -1,31 +1,41 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter, redirect} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import TodolistsPage from "./pages/TodolistsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+
+const loader = (path) => {
+  console.log(localStorage.getItem('access_token'));
+  localStorage.getItem("access_token") ? redirect('/todo') : redirect(path)
+}
+
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>
+    element: <HomePage/>,
+    
   },
   {
     path: "/signin",
-    element: <SignInPage/>
+    element: <SignInPage/>,
+    loader: loader('/signin')
+   
 
   },
   {
     path: "/signup",
-    element: <SignUpPage/>
+    element: <SignUpPage/>,
+   loader: loader('/signup')
 
   },
   {
-    path: "/todolists",
-    element: <TodolistsPage/>
-
+    path: "/todo",
+    element: <TodolistsPage/>,
+    loader : loader('/signin')
   },
-  // {
-  //   path: *,
-  //   element: <NotFoundPage/>
-  // }
+  {
+    path: "*",
+    element: <NotFoundPage/>
+  }
 ]);

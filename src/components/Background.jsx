1966213729d9c769acf05e75950f37c4efd9be98
youngeun <jessicak0828@gmail.com>
2023-06-08 04:12:22 +0900

    feat: Background 공통 컴포넌트 생성

---
+++ src/components/Background.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import * as S from '../styles/Background.styles'
+
+export default function Background({children}) {
+
+  return (
+    <S.Background>{children}</S.Background>
+  )
+}

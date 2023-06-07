895b34f8bb6fcfff06a1fa7be8a81feb1ef4177e
youngeun <jessicak0828@gmail.com>
2023-06-07 21:10:48 +0900

    design: HomePage 스타일

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import {PageButton} from '../components/Buttons'
+import Background from '../components/Background'
+import Container from '../components/Container'
+import Button from '../components/Buttons/Button'
+import { useNavigate} from 'react-router-dom'
+import thumbnail from '../assets/thumbnail.gif'
+ 
 export default function HomePage() {
+  const navigate = useNavigate();
+  const onClick = () => {
+  navigate('/signin')
+}
   return (
-    <>
-  <PageButton text={"시작하기"}/>
-    </>
+    <Background>
+      <Container>
+        <div className='image'><img src={thumbnail} alt="thumbnail" /></div>
+        <Button className='homeButton' onClick={onClick}>시작하기</Button>
+      </Container>
+    </Background>
   )
 }

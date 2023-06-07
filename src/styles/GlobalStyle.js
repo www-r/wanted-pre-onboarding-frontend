745a2367ce42003f11c3330b34c70dbb32f6038a
youngeun <jessicak0828@gmail.com>
2023-06-08 04:10:25 +0900

    feat: 폰트 적용

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+import '../assets/fonts/fonts.css'
+
+
+const GlobalStyle = createGlobalStyle`
+
+body {
+  font-family: "Humanbumsuk";
+}
+`
+export default GlobalStyle
+
+

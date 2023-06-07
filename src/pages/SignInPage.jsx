b0a971d1f1d843767f4780e0e62e39f16b56daff
youngeun <jessicak0828@gmail.com>
2023-06-08 04:17:04 +0900

    feat: 로그인 화면에서 엑세스 토큰 있으면 투두리스트 페이지로 바로 가고, 없으면 로그인 페이지에 있는다

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,11 +1,20 @@
-import React, {useState} from 'react'
+import React , {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import Background from '../components/Background'
+import Container from '../components/Container'
 import Sign from '../components/Sign'
 
 export default function SignInPage() {
+  const navigate = useNavigate()
+  useEffect(()=>{
+    localStorage.getItem('access_token') && navigate('/todo')
+  })
 
   return (
-    <>
-      <Sign title={'로그인'} action={'signin'}/>
-    </>
+    <Background>
+      <Container>
+        <Sign title={'로그인'} action={'signin'}/>
+      </Container>
+    </Background>
   )
 }

c8a9cfd8fc5dde4b00c3756f82c2e5b65d2290dc
youngeun <jessicak0828@gmail.com>
2023-06-08 04:24:34 +0900

    refactor : 컴포넌트 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React  from "react";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 import { Reset } from "styled-reset";
-
+import  GlobalStyle  from "./styles/GlobalStyle";
 
 function App() {
 
   return (
-    <>
-    <Reset/>
-   
       <div className="App">
-   
+        <Reset/>
+        <GlobalStyle/>
+        <RouterProvider router={router}/>
       </div>
-    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <>
- <RouterProvider router={router}/>
-    <App />
+
+    <App/>
   </>
 );
 
